71c59c85d6933f98eeb1d466035fa6c92e4c8ea7
cnguye-n <52482302+cnguye-n@users.noreply.github.com>
2025-10-23 21:01:40 -0700

    added updated files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Weather } from './weather/weather';
 import { Home } from './home/home';
 import { Country } from './country/country';
 import { City } from './city/city';
+import { CountryPopulation } from './country-population/country-population';
 
 export const routes: Routes = [
     { path: '', component: Home, pathMatch: 'full' },
     { path: 'weather', component: Weather },
     { path: 'city', component: City },
-    { path: 'country', component: Country }
+    { path: 'country', component: Country },
+    { path: 'country-population/:id', component: CountryPopulation }
 ];
